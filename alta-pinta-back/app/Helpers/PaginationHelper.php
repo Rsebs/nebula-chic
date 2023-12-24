211c25e6de42d2386b50d0ec68623bbf3fe836ed
Rsebs <sebastianruizj2014@gmail.com>
2023-12-24 02:48:47 -0500

    Backend | Agregado la ApiResponse y Helper para la paginación

---
+++ alta-pinta-back/app/Helpers/PaginationHelper.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Helpers;
+
+class PaginationHelper
+{
+  /**
+   * Create a formatted array containing pagination information.
+   *
+   * @param \Illuminate\Pagination\LengthAwarePaginator $paginate
+   *
+   * @return array
+   */
+  public static function createPagination($paginate)
+  {
+    $paginate = $paginate->toArray();
+
+    return [
+      "current_page" => $paginate["current_page"],
+      "first_page_url" => $paginate["first_page_url"],
+      "from" => $paginate["from"],
+      "last_page" => $paginate["last_page"],
+      "last_page_url" => $paginate["last_page_url"],
+      "links" => $paginate["links"],
+      "next_page_url" => $paginate["next_page_url"],
+      "per_page" => $paginate["per_page"],
+      "prev_page_url" => $paginate["prev_page_url"],
+      "to" => $paginate["to"],
+      "total" => $paginate["total"],
+    ];
+  }
+}
---
+++ alta-pinta-back/app/Traits/ApiResponse.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponse
+{
+  /**
+   * Generates a JSON response for a successful operation.
+   * 
+   * @param mixed $data
+   * @param string $message
+   * @param int $status
+   * 
+   * @return \Illuminate\Http\JsonResponse
+   */
+  protected function successResponse($data, $paginate = [], $message = "Success!", $status = 200)
+  {
+    return response()->json([
+      "message" => $message,
+      "statusCode" => $status,
+      "data" => $data,
+      "paginate" => $paginate,
+    ], $status);
+  }
+
+  /**
+   * Generate a JSON response for error scenarios.
+   * 
+   * @param mixed $data
+   * @param string $message
+   * @param int $status
+   * 
+   * @return \Illuminate\Http\JsonResponse
+   */
+  protected function errorResponse($error = [], $message = "Error.", $status = 500)
+  {
+    return response()->json([
+      "message" => $message,
+      "statusCode" => $status,
+      "errors" => $error,
+    ], $status);
+  }
+}
