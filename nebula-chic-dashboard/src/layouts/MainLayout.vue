96aa8e94419156f5128fed1963df76f71434ff9b
Rsebs <sebastianruizj2014@gmail.com>
2024-05-25 15:56:17 -0500

    Dashboard | MainLayout agregado para la aplicación.

--- nebula-chic-dashboard/src/layouts/MainLayout.vue
+++ nebula-chic-dashboard/src/layouts/MainLayout.vue
@@ -1,5 +1,60 @@
 <template>
-  <router-view></router-view>
+  <v-app>
+    <v-layout>
+      <v-navigation-drawer>
+        <v-list>
+          <v-list-item
+            :prepend-avatar="user.avatar"
+            :title="user.name"
+            :subtitle="user.email"
+          />
+        </v-list>
+
+        <v-divider />
+
+        <v-list density="compact" nav>
+          <v-list-item
+            v-for="item in itemsList"
+            :key="item.title"
+            :to="item.to"
+            :title="item.title"
+            :prepend-icon="item.icon"
+          />
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <v-container>
+          <router-view />
+        </v-container>
+      </v-main>
+    </v-layout>
+
+    <AppFooter />
+  </v-app>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import AppFooter from '@/components/AppFooter.vue';
+
+import { ref } from 'vue';
+
+const user = ref({
+  name: 'Sebs',
+  avatar: 'https://github.com/rsebs.png',
+  email: 'sebastianruizj2014@gmail.com',
+});
+
+const itemsList = ref([
+  {
+    title: 'Index',
+    icon: 'mdi-home',
+    to: { name: 'Index' },
+  },
+  {
+    title: 'About',
+    icon: 'mdi-help',
+    to: { name: 'About' },
+  },
+]);
+</script>
