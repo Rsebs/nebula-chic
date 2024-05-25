7b7495fb40f0899a5bcfcd09f33dcbc0f3597b32
Rsebs <sebastianruizj2014@gmail.com>
2024-05-25 15:26:06 -0500

    Dashboard | Vuetify instalado.

--- nebula-chic-dashboard/src/layouts/MainLayout.vue
+++ nebula-chic-dashboard/src/layouts/MainLayout.vue
@@ -1,10 +1,4 @@
 <template>
-  <button>
-    <router-link :to="{ name: 'Index' }"> Index </router-link>
-  </button>
-  <button>
-    <router-link :to="{ name: 'About' }"> About </router-link>
-  </button>
   <router-view></router-view>
 </template>
 
--- nebula-chic-dashboard/src/main.ts
+++ nebula-chic-dashboard/src/main.ts
@@ -1,3 +1,10 @@
+// Vuetify
+import '@mdi/font/css/materialdesignicons.css';
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
 import './assets/main.css';
 
 import { createApp } from 'vue';
@@ -6,8 +13,15 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
+const vuetify = createVuetify({
+  components,
+  directives,
+  ssr: true,
+});
+
 const app = createApp(App);
 
+app.use(vuetify);
 app.use(createPinia());
 app.use(router);
 
